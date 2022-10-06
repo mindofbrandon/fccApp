5467fa2f10be46df626dd1713980c6fd4aecc7ba
Brandon Zuniga <brandon.zuniga@fmr.com>
2022-10-06 15:28:34 -0400

    import httpclient, create homecomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './home/home.component'; // add httpclient to consume rest api
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule // declare httpclient import
   ],
   providers: [],
   bootstrap: [AppComponent]
