819c768e9c91f477f8f683e68039de7cf2bb2a77
Brandon Zuniga <brandon.zuniga@fmr.com>
2022-10-06 15:38:54 -0400

    add routing to home, about components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'}, // redirect empty path to home component upon first visit of app
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { HomeComponent } from './home/home.component'; // add httpclient to consume rest api
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component'; // add httpclient to consume rest api
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
