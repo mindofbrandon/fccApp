072ccee5ffc61e888e4adf72a868adea8dbf6144
Brandon Zuniga <brandon.zuniga@fmr.com>
2022-10-06 16:10:13 -0400

    add Angular Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'; // add httpclient to consume rest api
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // add httpclient to consume rest api
+import {MatToolbarModule} from '@angular/material/toolbar'; // container for headers titles and actions
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card'; // container for text photos and actions of a single subject
+import {MatButtonModule} from '@angular/material/button'; // native <button> and <a> elements with material design styling
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';// circular indicator of progress and activity
 
 @NgModule({
   declarations: [
@@ -16,7 +22,14 @@ import { AboutComponent } from './about/about.component'; // add httpclient to c
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule // declare httpclient import
+    HttpClientModule,
+    BrowserAnimationsModule, // declare httpclient import
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
