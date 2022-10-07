b73575c46c4f496581edaa2e4516043ec39a0412
Brandon Zuniga <brandon.zuniga@fmr.com>
2022-10-07 15:15:46 -0400

    consume rest api changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service'; //import and inject ApiService
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +7,14 @@ import { ApiService } from '../api.service'; //import and inject ApiService
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  products: any; // originally products = any[];
-  constructor(private apiService: ApiService) { }
 
-  ngOnInit() {
-    this.apiService.sendGetRequest().subscribe((data: any)=>{ // originally: data: any[]
-      console.log(data);
-      this.products = data;
+  products: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:3000/products').subscribe(data => { // <any> typecast is CRUCIAL else home.compononent.html will not be able to access product properties
+      console.log('data', JSON.stringify(data))
+      this.products = data
     })
   }
 }
