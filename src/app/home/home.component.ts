77893b96927cc34b856ba18a9c1e34a3b39bbc46
Brandon Zuniga <brandon.zuniga@fmr.com>
2022-10-07 09:39:15 -0400

    api request with error handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service'; //import and inject ApiService
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  products: any; // originally products = any[];
+  constructor(private apiService: ApiService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apiService.sendGetRequest().subscribe((data: any)=>{ // originally: data: any[]
+      console.log(data);
+      this.products = data;
+    })
   }
-
 }
